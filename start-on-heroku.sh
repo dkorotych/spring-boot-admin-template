95ee269c5e779e4d8b436e7818f0dde3e97c6f65
Dmitry Korotych <dkorotych@gmail.com>
2019-05-17 22:13:54 +0300

    Change archive name

--- start-on-heroku.sh
+++ start-on-heroku.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-java -Dserver.port=${PORT} -jar target/admin-console.jar
+java -Dserver.port=${PORT} -jar build/libs/admin-console.jar
 if [ -f hs_err_pid*.log ]; then
   cat hs_err_pid*.log
 fi
