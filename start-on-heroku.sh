8c947c9ede59509572f4d480cc698c65d41eeb13
Dmitry Korotych <dkorotych@gmail.com>
2021-09-13 15:53:32 +0300

    -f doesn't work with globs. Use a for loop.

--- start-on-heroku.sh
+++ start-on-heroku.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 java -Dserver.port=${PORT} -jar build/libs/admin-console.jar
-if [ -f hs_err_pid*.log ]; then
-  cat hs_err_pid*.log
-fi
+for file in hs_err_pid*.log; do
+  if [ -f "$file" ]; then
+    cat hs_err_pid*.log
+    break
+  fi
+done
