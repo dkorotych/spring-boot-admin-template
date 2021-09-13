d8e88b91d28a8e875ac1c00a4906e84af4989b53
Dmitry Korotych <dkorotych@gmail.com>
2021-09-13 15:54:14 +0300

    Double quote to prevent globbing and word splitting.

--- start-on-heroku.sh
+++ start-on-heroku.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-java -Dserver.port=${PORT} -jar build/libs/admin-console.jar
+java -Dserver.port="${PORT}" -jar build/libs/admin-console.jar
 for file in hs_err_pid*.log; do
   if [ -f "$file" ]; then
     cat hs_err_pid*.log
